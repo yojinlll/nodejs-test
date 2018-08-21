3c2ce64b3b9687faf2cb2e34c2919bea07bb63c0
yojinlll <yojin1995@qq.com>
2018-08-21 22:00:31 +0800

    优化

--- main.js
+++ main.js
@@ -5,9 +5,21 @@ window.jQuery = function(nodeOrSelector){
     return nodes
 }
 
-window.jQuery.ajax = function(url,method,body,successFn,failFn){
+window.jQuery.ajax = function(options){
+    let url = options.url
+    let method = options.method
+    let body = options.body
+    let successFn = options.successFn
+    let failFn = options.failFn
+    let headers = options.headers
+    // ES6 解构赋值 : let {url,method,body,successFn,failFn} = options
+    
     let request = new XMLHttpRequest()
     request.open(method,url)
+    for( let key in headers){
+        let value = headers[key]
+        request.setRequestHeader(key,value)
+    }
     request.onreadystatechange = ()=>{
         if(request.readyState === 4){
             if(request.status >= 200 && request.status < 300){
@@ -23,11 +35,16 @@ window.jQuery.ajax = function(url,method,body,successFn,failFn){
 window.$ = window.jQuery
 
 myButton.addEventListener('click',(e)=>{
-    window.jQuery.ajax(
-        '/xxx',
-        'post',
-        'a=1&b=2',
-        (responseText)=>{console.log(1)},
-        (request)=>{console.log(2)}
-    )
+    window.jQuery.ajax({
+        url:'/xxx',
+        method:'get',
+        headers:{
+            'Content-Type':'application/x-www-form-urlencoded',
+            'frank':'18'
+        },
+        successFn:()=>{
+            console.log(123)
+        },
+        failFn:()=>{}    
+    })
 })
