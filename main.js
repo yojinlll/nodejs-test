b60504cef7c174161352f05d4d8ed129ab07e0c6
yojinlll <yojin1995@qq.com>
2018-08-22 11:44:22 +0800

    提交promise优化

--- main.js
+++ main.js
@@ -4,47 +4,39 @@ window.jQuery = function(nodeOrSelector){
     nodes.html = function(){}
     return nodes
 }
+window.$ = window.jQuery
 
-window.jQuery.ajax = function(options){
-    let url = options.url
-    let method = options.method
-    let body = options.body
-    let successFn = options.successFn
-    let failFn = options.failFn
-    let headers = options.headers
-    // ES6 解构赋值 : let {url,method,body,successFn,failFn} = options
-    
-    let request = new XMLHttpRequest()
-    request.open(method,url)
-    for( let key in headers){
-        let value = headers[key]
-        request.setRequestHeader(key,value)
-    }
-    request.onreadystatechange = ()=>{
-        if(request.readyState === 4){
-            if(request.status >= 200 && request.status < 300){
-                successFn.call(undefined,request.responseText)
-            }else if(request.status >= 400){
-                failFn.call(undefined,request)
+window.jQuery.ajax = function({url,method,body,headers}){
+    return new Promise(function(resolve,reject){
+        let request = new XMLHttpRequest()
+        request.open(method,url)
+        for( let key in headers){
+            let value = headers[key]
+            request.setRequestHeader(key,value)
+        }
+        request.onreadystatechange = ()=>{
+            if(request.readyState === 4){
+                if(request.status >= 200 && request.status < 300){
+                    resolve.call(undefined,request.responseText)
+                }else if(request.status >= 400){
+                    reject.call(undefined,request)
+                }
             }
         }
-    }
-    request.send(body)
+        request.send(body)
+    })    
 }
-
-window.$ = window.jQuery
 
 myButton.addEventListener('click',(e)=>{
     window.jQuery.ajax({
         url:'/xxx',
         method:'get',
         headers:{
-            'Content-Type':'application/x-www-form-urlencoded',
+            'Content-Type':'application/x-www-form-urlcoded',
             'frank':'18'
-        },
-        successFn:()=>{
-            console.log(123)
-        },
-        failFn:()=>{}    
-    })
+        }
+    }).then(
+            (text)=>{console.log(text)},
+            (request)=>{console.log('error')}
+        )
 })
